e017a692904c5f52b358ada10d9cab47a1126397
Invader Zim <pwstern@gmail.com>
2020-03-23 14:57:29 -0700

    Added scroll view and image view with picture of canyon bike.

--- EyeScan/EyeScanViewController.swift
+++ EyeScan/EyeScanViewController.swift
@@ -10,11 +10,12 @@ import UIKit
 
 class EyeScanViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        imageView.image = UIImage(named: "canyon")
     }
-
 
 }
 
