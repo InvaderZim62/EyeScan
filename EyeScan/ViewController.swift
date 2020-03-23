0ae701f91583a96bc0f05f0dd0b54f2edc5545a5
Invader Zim <pwstern@gmail.com>
2020-03-23 14:50:31 -0700

    Initial Commit

---
+++ EyeScan/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EyeScan
+//
+//  Created by Phil Stern on 3/23/20.
+//  Copyright © 2020 Phil Stern. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
